3cc16f0fea2774dcdabb7af071b7f9e5714186c2
Ivar Grytten <ivar.grytten@gmail.com>
2018-02-15 14:18:41 +0100

    Dev

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 ./generate_paper_figures_and_tables.sh  ENCSR000DUB 1 CTCF 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X https://www.encodeproject.org/files/ENCFF639IFG/@@download/ENCFF639IFG.bam http://jaspar.genereg.net/api/v1/matrix/MA0139.1.meme
+./generate_paper_figures_and_tables.sh  ENCSR000DUB 1 CTCFNonTrimming 15,16,17,18,19,20,21,22 https://www.encodeproject.org/files/ENCFF639IFG/@@download/ENCFF639IFG.bam http://jaspar.genereg.net/api/v1/matrix/MA0139.1.meme
+
+./generate_paper_figures_and_tables.sh  ENCSR521IID 1 MAX 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22 https://www.encodeproject.org/files/ENCFF499VEN/@@download/ENCFF499VEN.bam http://jaspar.genereg.net/api/v1/matrix/MA0058.2.meme
 
 ./generate_paper_figures_and_tables.sh  ENCSR000BIV 1 SRF 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X https://www.encodeproject.org/files/ENCFF575DNN/@@download/ENCFF575DNN.bam http://jaspar.genereg.net/api/v1/matrix/MA0083.2.meme
 
@@ -11,10 +14,16 @@
 
 ./generate_paper_figures_and_tables.sh  ENCSR070VXO 1 NR3C1 10,11,12,13,14,15,16,17,18,19,20,21,22 https://www.encodeproject.org/files/ENCFF883JID/@@download/ENCFF883JID.bam http://jaspar.genereg.net/api/v1/matrix/MA0113.3.meme
 
-./generate_paper_figures_and_tables.sh  ENCSR000BMZ 1 ELF1 19,20,21,22 https://www.encodeproject.org/files/ENCFF418EVV/@@download/ENCFF418EVV.bam http://jaspar.genereg.net/api/v1/matrix/MA0473.2.meme
+./generate_paper_figures_and_tables.sh  ENCSR000BMZ 1 ELF1 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22 https://www.encodeproject.org/files/ENCFF418EVV/@@download/ENCFF418EVV.bam http://jaspar.genereg.net/api/v1/matrix/MA0473.2.meme
 
-./generate_paper_figures_and_tables.sh  ENCSR000ECD 1 USF2 9,10,11,12,13,14,15,16,17,18,19,20,21,22 https://www.encodeproject.org/files/ENCFF992YZZ/@@download/ENCFF992YZZ.bam http://jaspar.genereg.net/api/v1/matrix/MA0526.1.meme
+./generate_paper_figures_and_tables.sh  ENCSR000ECD 1 USF2 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22 https://www.encodeproject.org/files/ENCFF992YZZ/@@download/ENCFF992YZZ.bam http://jaspar.genereg.net/api/v1/matrix/MA0526.1.meme
 
 ./generate_paper_figures_and_tables.sh  ENCSR000ECD 1 SP1 17 https://www.encodeproject.org/files/ENCFF992YZZ/@@download/ENCFF992YZZ.bam http://jaspar.genereg.net/api/v1/matrix/MA0526.1.meme
 
 ./generate_paper_figures_and_tables.sh  ENCSR000EXZ 1 GATA3 12,13,14,15,16,17,18,19,20,21,22,X https://www.encodeproject.org/files/ENCFF318YMR/@@download/ENCFF318YMR.bam http://jaspar.genereg.net/api/v1/matrix/MA0037.2.memes
+
+
+
+
+# Using raw fastq and performing linear mapping
+./generate_paper_figures_and_tables.sh  ENCSR000DUB 1 CTCFCustomMapping 15,16,17,18,19,20,21,22 http://jaspar.genereg.net/api/v1/matrix/MA0139.1.meme
