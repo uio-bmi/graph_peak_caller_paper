48f28a2af23911ef8891368f1a8bcc5c8fb66815
Ivar Grytten <ivar.grytten@gmail.com>
2018-03-03 12:17:27 +0100

    Bash shebang

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 ./call_peaks.sh blank_config.sh ENCSR471GSA 1 DM_JRA chr3R,chr3L,chr2R,chr2L,chrX,chr4 https://www.encodeproject.org/files/ENCFF597CVA/@@download/ENCFF597CVA.bam . 97958719 /media/storage1/dm6/dm3_main_chromosomes.fasta /media/storage1/dm6/wg.xg /media/storage1/dm6/wg.gcsa /media/storage1/dm6/  > dm_jra_output.txt 2 >&1 &
 ./call_peaks.sh blank_config.sh ENCSR923VWW 1 DM_SQZ chr3R,chr3L,chr2R,chr2L,chrX,chr4 https://www.encodeproject.org/files/ENCFF255JBC/@@download/ENCFF255JBC.bam . 97958719 /media/storage1/dm6/dm3_main_chromosomes.fasta /media/storage1/dm6/wg.xg /media/storage1/dm6/wg.gcsa /media/storage1/dm6/ > dm_sqz_output.txt 2 >&1 &
